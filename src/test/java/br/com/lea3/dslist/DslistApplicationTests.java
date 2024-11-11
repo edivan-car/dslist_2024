0c2a234435d5820b3a013a1ae368511dd6e39b4d
edivan-car <edivam.c@gmail.com>
2024-11-11 12:02:21 -0300

    Project created

---
+++ src/test/java/br/com/lea3/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lea3.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
